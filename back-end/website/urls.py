d987046f551075696347fead0f0dc05c84ed5851
Devin Villarosa <DevinVillarosa92@gmail.com>
2017-12-04 13:42:17 -0800

    Removed accounts app due to rest-auth handling it

--- back-end/website/urls.py
+++ back-end/website/urls.py
@@ -3,9 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^rest-auth/', include('rest_auth.urls')),
-    url(r'^rest-auth/', include('rest_auth.urls')),
     url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('accounts.urls')),
     url(r'^', include('fitness.urls')),
 ]
