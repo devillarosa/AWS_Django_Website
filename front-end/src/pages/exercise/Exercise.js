4185c89ce7e88b8b3b079b18f76bbdb036460d72
Devin Villarosa <DevinVillarosa92@gmail.com>
2017-12-06 13:13:43 -0800

    Addings pages for router

---
+++ front-end/src/pages/exercise/Exercise.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Exercise.css';
+
+class Exercise extends Component {
+  render() {
+    return (
+      <div className="Exercise">
+        <h1> Exercise Page </h1>
+      </div>
+    );
+  }
+}
+
+export default Exercise;
---
+++ front-end/src/pages/workout/Workout.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Workout.css';
+
+class Workout extends Component {
+  render() {
+    return (
+      <div className="Workout">
+        <h1> Workout Page </h1>
+      </div>
+    );
+  }
+}
+
+export default Workout;
