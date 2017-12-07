1e89459dd34781592d0feebc07e5c4b9f296c692
Devin Villarosa <devillarosa@Devins-MacBook-Pro.local>
2017-12-06 18:24:10 -0800

    Simple implementation with react router

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,20 +2,35 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Link, Route, Switch } from 'react-router-dom'
+
+import { BrowserRouter } from 'react-router-dom';
+import  Home from './pages/home/Home';
+import Exercise from './pages/exercise/Exercise'; 
+import Workout from './pages/workout/Workout'; 
+import NotFound from './pages/notfound/NotFound'; 
+
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
 
+    return (
+      <div>
+        <nav className="navbar navbar-light">
+          <ul className="nav navbar-nav">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/exercise">Exercise</Link></li>
+            <li><Link to="/workout">Workouts</Link></li>
+          </ul> 
+        </nav>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route path="/exercise" component={Exercise}/>
+          <Route path="/workout" component={Workout}/> 
+          <Route path="*" component={NotFound}/> 
+        </Switch>
+
+        </div>
+       );
+    }
+}
 export default App;
